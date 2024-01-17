98ddb0fae52071a08bd43c6c099d7ada8c2c3888
Joel Berg <bergjoel93@gmail.com>
2024-01-17 09:03:15 -0700

    This is my first attemp to try to solve the problem.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, count) {
+    if(count < 0){
+        return 'ERROR';
+    }
 
+    let result = "";
+    for(let i = 0; i < count; i++){
+        result = result+str;
+    }
+    return result;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, toRemove) {
+    //obtain index of element to be removed
+    let index = array.indexOf(toRemove);
+    //loop through array and build new array but skip the index of the element
+    //to be removed. 
+    let newArray = [];
+    for(let i = 0; i < array.length; i++){
+        if(i === index) continue;
+        newArray.push(array[i]);
+    }
+    return newArray;
+    
 };
 
 // Do not edit below this line
